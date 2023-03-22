2fedc33d2dfb86dc2b10b79d6687462351736950
EC2 Default User <ec2-user@ip-172-31-45-195.us-east-2.compute.internal>
2023-03-22 14:17:15 +0000

    program to move files to the home dir

---
+++ mv-to-home.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+while true; do
+  read -p "zsh or bash? (zsh/bash):" env_type
+  if [ "$env_type" = "zsh" ] || [ "$env_type" = "bash" ]; then
+    break
+  else
+    echo "Invalid input. Please try again."
+  fi
+done
+
+if [ "$env_type" = "bash" ]; then
+  rm .zshrc
+else
+  rm .bashrc
+fi
+
+mv *. ~/
