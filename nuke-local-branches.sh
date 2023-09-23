e75ff8d471fad18d50e8e71c58730c66980e77f1
Curtis Chong <curtischong5@gmail.com>
2023-09-23 11:16:43 -0400

    nuke local branches script is much faster since it now only makes one request

--- nuke-local-branches.sh
+++ nuke-local-branches.sh
@@ -7,24 +7,41 @@
 # you want to keep. The script will delete the other branches
 
 
-# Fetch the remote branches
+# Fetch all remote branches
 git fetch
 
+# Get remote branches
+remote_branches=$(git branch -r --format="%(refname:short)" | awk -F '/' '{print $2}')
 
-# Get all local branches
+# Get local branches
 local_branches=$(git branch --format="%(refname:short)")
+
+# Convert remote branches to an array
+IFS=$'\n' read -r -d '' -a remotes <<< "$remote_branches"
 
 # Filter branches that are missing on the remote repo
 filtered_branches=()
 
 # Iterate over local branches
-for branch in $local_branches
+for local in $local_branches
 do
-   # Check if the local branch exists on the remote
-   if ! git ls-remote --exit-code --heads origin "$branch" &>/dev/null
-   then
-       filtered_branches+=("$branch")
-   fi
+    found=0
+    # Check if branch exists on remote
+    for remote in "${remotes[@]}"
+    do
+        if [[ $local == $remote ]]
+        then
+            # Branch found on remote
+            found=1
+            break
+        fi
+    done
+
+    # Branch not found on remote
+    if ((found == 0))
+    then
+        filtered_branches+=("$local")
+    fi
 done
 
 
@@ -47,7 +64,7 @@ while true; do
         echo "Invalid choice. Please try again."
     elif [[ ! " ${branches_to_keep[@]} " =~ " $branch_number " ]]; then
         branches_to_keep+=($branch_number)
-        echo "-------Branch kept: ${filtered_branches[$branch_number-1]}"
+        echo "Branch kept: ${filtered_branches[$branch_number-1]}"
 
         echo "-------The branches you are keeping are:"
         for i in "${branches_to_keep[@]}"; do
