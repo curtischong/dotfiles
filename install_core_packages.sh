95af8ecd5e13750f5b38cc5c242ba227205530d7
Curtis Chong <curtischong5@gmail.com>
2024-02-27 16:15:38 -0500

    rm init

--- install_core_packages.sh
+++ install_core_packages.sh
@@ -117,7 +117,7 @@ mkdir -p ~/.local/bin && ln -s $(which fdfind) ~/.local/bin/fd # link the fd-fin
 # install zoxide
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 export PATH="$HOME/.local/bin:$PATH"
-eval "$(zoxide init $(basename $SHELL))" # use $SHELL to make this work for zsh or bash
+zoxide init $(basename $SHELL) # use $SHELL to make this work for zsh or bash
 
 
 
